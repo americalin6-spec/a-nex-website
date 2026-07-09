b6eb8824cee2662fffa3056d1c52e3120967fde9
Chris Lin <americalin6@gmail.com>
2026-07-09 10:25:14 +0800

    redirect platform to line-ai

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/platform",
+        destination: "/line-ai",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
